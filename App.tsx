0a017681eb8270063993dac7c0948b75e5e8a022
Novsochetra <sochetra12.nov@gmail.com>
2020-05-18 15:43:59 +0700

    update read me scroll stack card

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React, {ReactElement} from 'react'
 import {} from 'react-native'
-// import {ScrollViewImagesGallery} from './src/components/scrollview-images-gallery'
+import {ScrollViewImagesGallery} from './src/components/scrollview-images-gallery'
 import {ScrollTransformCard} from './src/components/scroll-transform-card'
-// import {ScrollStackCard} from './src/components/scroll-stack-card'
+import {ScrollStackCard} from './src/components/scroll-stack-card'
 
 export const App = (): ReactElement => {
   // return <ScrollViewImagesGallery />
-  return <ScrollTransformCard />
-  // return <ScrollStackCard />
+  // return <ScrollTransformCard />
+  return <ScrollStackCard />
 }
 
 export default App
---
+++ src/components/scroll-stack-card/README.md
@@ -0,0 +1,1 @@
+![scroll stack video](https://github.com/Novsochetra/Animation-React-Native/blob/master/src/components/scroll-transform-card/scroll-stack-card.gif)
